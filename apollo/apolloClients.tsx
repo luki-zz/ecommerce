e77464defbb28c759e2287a75bffafd80030b819
luki-zz <luki.wojnar@gmail.com>
2023-02-26 18:25:30 +0100

    initial commit

---
+++ apollo/apolloClients.tsx
@@ -0,0 +1,7 @@
+import { ApolloClient, InMemoryCache } from "@apollo/client";
+const API_URL = process.env.HYGRAPH_API;
+
+export const client = new ApolloClient({
+  uri: "https://api-eu-west-2.hygraph.com/v2/cleityuh0096q01tadtcog76x/master",
+  cache: new InMemoryCache(),
+});
