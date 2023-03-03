b581a705edda2c4705bd5a81320789aa6ceef9a8
luki-zz <luki.wojnar@gmail.com>
2023-03-03 21:10:25 +0100

    add basic Layout structure

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,10 +2,13 @@ import "styles/globals.css";
 import type { AppProps } from "next/app";
 import { ApolloProvider } from "@apollo/client";
 import { client } from "apollo/apolloClients";
+import Layout from "src/components/layout";
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <ApolloProvider client={client}>
-      <Component {...pageProps} />
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
     </ApolloProvider>
   );
 }
