931ddc9d518acecb74b29a4987e761fec5964522
luki-zz <luki.wojnar@gmail.com>
2023-03-05 16:49:07 +0100

    add getEnvVar

--- apollo/apolloClients.tsx
+++ apollo/apolloClients.tsx
@@ -2,6 +2,10 @@ import { ApolloClient, InMemoryCache } from "@apollo/client";
 
 const GRAPHQL_URL = process.env.NEXT_PUBLIC_HYGRAPH_API;
 
+const getEnvVar = (var) => {
+  return `process.env.${var}`
+}
+
 export const client = new ApolloClient({
   uri: GRAPHQL_URL,
   cache: new InMemoryCache(),
